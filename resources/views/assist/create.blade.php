a4fa467b4b156640db4aede4079287d787b33d2d
OrlyIdeate <ueno.jin25@outlook.jp>
2023-10-15 14:26:46 +0900

    Navigationにindexリンクを追加

--- resources/views/assist/create.blade.php
+++ resources/views/assist/create.blade.php
@@ -11,25 +11,29 @@
         <div class="grid grid-cols-2 gap-4 max-w-7xl mx-auto sm:w-11/12 md:w-11/12 lg:w11/12">
             <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-800 ">
                 <form class="mb-6" action="{{ route('assist.store') }}" method="POST"enctype="multipart/form-data">
-                     @csrf
+                    @csrf
                     <div class="flex flex-col mb-4">
                         <x-input-label for="question" :value="__('問題')" />
-                        <textarea class="rounded resize-none",type=question name="question" id="question":value="old('question')" cols="30" rows="8" required autofocus></textarea>
-                        <!-- <x-input-error :messages="$errors->get('question')" class="mt-2" /> -->
+                        <textarea class="rounded resize-none",type=question name="question" id="question":cols="30" rows="8" required
+                            autofocus>{{ old('question') }}</textarea>
+                        <x-input-error :messages="$errors->get('question')" class="mt-2" />
                     </div>
                     <div class="flex flex-col mb-4">
                         <x-input-label for="criterion" :value="__('採点基準')" />
                         <a onclick=add() class="btn btn-sm btn-light">採点基準の追加</a>
                         <div id="input_plural">
                             <div class="d-flex">
-                                <input type="text" class="form-control mb-1" name="criterion">
-                                <input type="button" value="削除" onclick="del(this)">
+                                <input type="text" class="form-control mb-1 w-full" name="criterion">
+                                <input
+                                    class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                                    type="button" value="削除" onclick="del(this)">
                             </div>
                         </div>
                     </div>
                     <div class="flex flex-col mb-4">
                         <x-input-label for="answer" :value="__('生徒の回答')" />
-                        <textarea class="rounded resize-none",type=answer name="answer" id="answer":value="old('answer')" cols="30" rows="8" required autofocus></textarea>
+                        <textarea class="rounded resize-none",type=answer name="answer" id="answer": cols="30" rows="8" required
+                            autofocus>{{ old('answer') }}</textarea>
                         <!-- <x-input-error :messages="$errors->get('answer')" class="mt-2" /> -->
                     </div>
                     <div class="flex flex-col items-center">
@@ -37,13 +41,13 @@
                             {{ __('Create') }}
                         </x-primary-button>
                     </div>
-                </form>              
+                </form>
             </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white dark:bg-gray-800 flex flex-col">
                     <!-- ↓ 採点結果（仮）↓ -->
                     <label class="dark:text-white" for="result">解答の評価や修正点</label>
-                    <textarea class="rounded resize-none" name="result" id="result" cols="30" rows="32"></textarea>
+                    <textarea class="rounded resize-none" name="result" id="result" cols="30" rows="32">{{ old('result') }}</textarea>
                     <!-- ↑ 採点結果（仮）↑  -->
                 </div>
             </div>
