5ef00caca2a9fcf5f130d20991beb84a1cc704eb
OrlyIdeate <ueno.jin25@outlook.jp>
2023-10-21 15:55:28 +0900

    マイグレーションにnameを追加

--- database/migrations/2023_10_14_054121_create_assists_table.php
+++ database/migrations/2023_10_14_054121_create_assists_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('question');
             $table->string('criterion');
+            $table->string('name');
             $table->string('answer');
             $table->string('result');
             $table->timestamps();
